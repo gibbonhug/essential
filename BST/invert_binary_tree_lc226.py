94cc19c44450d681c5ea393694f168a48c99b6f9
Julian <juliangwillis@gmail.com>
2022-10-30 07:29:59 -0400

    Create invert_binary_tree, complete recursive version

---
+++ BST/invert_binary_tree_lc226.py
@@ -0,0 +1,33 @@
+# Invert Binary Tree
+# Leetcode 226
+# https://leetcode.com/problems/invert-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def invert_binary_tree_recursive_lc226(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    """Given the root of a binary tree, invert the tree, and return its root.
+
+    :param root: The root node of the binary tree to invert
+    :type root: Optional[TreeNode]
+
+    :rtype: Optional[TreeNode]
+    :return: The root node of the binary tree
+    """
+    if root is None:
+        return None
+
+    temp_node: Optional[TreeNode] = root.left
+    root.left = root.right
+    root.right = temp_node
+
+    # recursively invert child nodes
+    invert_binary_tree_recursive_lc226(root.left)
+    invert_binary_tree_recursive_lc226(root.right)
+
+    return root
+    
