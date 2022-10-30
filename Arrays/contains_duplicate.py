7633fd45c8c175a569f3847d3a16e49825f52396
Julian <juliangwillis@gmail.com>
2022-10-30 06:56:52 -0400

    Update comments for contains_duplicate

--- Arrays/contains_duplicate.py
+++ Arrays/contains_duplicate.py
@@ -1,11 +1,15 @@
-# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Contains duplicate
 # Leetcode 217
 # https://leetcode.com/problems/contains-duplicate/
 
-def containsDuplicate(nums: list[int]) -> bool:
-    """
+def containsDuplicate(nums: List[int]) -> bool:
+    """Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+    :param arr: The list of ints to search
     :type nums: List[int]
+
     :rtype: bool
+    :return: Boolean corresponding to whether nums contains duplicate values
     """
     seen_nums = set()
 
