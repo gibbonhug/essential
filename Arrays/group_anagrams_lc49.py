55d1ac1583bff5bcad176d678dfb54ad8fbfb3f9
Julian <juliangwillis@gmail.com>
2022-11-06 18:57:48 -0500

    Make Group Anagrams file, fix name of 2 sum 2 func

--- Arrays/group_anagrams_lc49.py
+++ Arrays/group_anagrams_lc49.py
@@ -1,8 +1,8 @@
-# Two Sum II
-# Leetcode 67
+# Group Anagrams
+# Leetcode 49
 # https://leetcode.com/problems/two-sum/
 
-def two_sum__ii_lc167(nums: List[int], target: int) -> List[int]:
+def two_sum_lc167(nums: List[int], target: int) -> List[int]:
     """Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
     Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
