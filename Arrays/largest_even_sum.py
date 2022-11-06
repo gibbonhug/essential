da528d273b00b235ae0d5509add8aefc640bd983
Julian <juliangwillis@gmail.com>
2022-11-06 15:14:35 -0500

    Add documentation to largest_even_sum, clean up text, and fix missing return statement

--- Arrays/largest_even_sum.py
+++ Arrays/largest_even_sum.py
@@ -1,15 +1,27 @@
-def largest_even_sum(arr: List[int]) -> List[int]:
+def largest_even_sum(arr: List[int]) -> int:
+    """Given an integer array arr, return the greatest possible even sum which can be summed from elements of arr.
+
+    :param arr: The list of nums
+    :type arr: List[int]
+
+    :rtype: int
+    :return: Largest possible even sum which can be formed from elements of arr
+    """
     running_total: int = 0
 
-    prev_odd: int = 0
+    # 'stack' of odd nums; no need to make an actual stack since we are sorting the array,
+    # so it is easy to know when adding 2 odds will increase the running_total sum
+    prev_odd: int = 0 
     this_odd: int = 0
     num_odd: int = 0
 
     arr.sort()
 
     for num in reversed(arr):
+        # an even number which will increase running total
         if (num % 2 == 0) and (running_total + num > running_total):
             running_total += num
+        # odd
         elif (num % 2 == 1):
             prev_odd = this_odd
             this_odd = num
@@ -17,11 +29,14 @@ def largest_even_sum(arr: List[int]) -> List[int]:
 
         if num_odd == 2 and this_odd + prev_odd > 0:
             running_total += this_odd
-            this_odd = 0
             running_total += prev_odd
+            # empty 'stack'
+            this_odd = 0
             prev_odd = 0
             num_odd = 0
         
         # no use going down into negative numbers
-        if (num < 0) and (prev_odd < 0):
+        if (num < 0) and (prev_odd <= 0):
             break
+    
+    return running_total
