47d00376ff4b4d0c85cdfb5eeab55e0a87612f82
Julian <juliangwillis@gmail.com>
2022-11-06 18:55:37 -0500

    Fix number of LC problem in two sum 2 func name

--- Arrays/two_sum_ii_lc167.py
+++ Arrays/two_sum_ii_lc167.py
@@ -2,7 +2,7 @@
 # Leetcode 67
 # https://leetcode.com/problems/two-sum/
 
-def two_sum_lc67(nums: List[int], target: int) -> List[int]:
+def two_sum_lc167(nums: List[int], target: int) -> List[int]:
     """Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
     Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
