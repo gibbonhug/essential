d5ac5debcd651af2b115972a83f241afc0d4b153
Julian <juliangwillis@gmail.com>
2022-11-06 17:45:56 -0500

    Recursive valid_palindrome finished

--- Strings/valid_palindrome_lc125.py
+++ Strings/valid_palindrome_lc125.py
@@ -1,3 +1,5 @@
+import re
+
 # Valid Palindrome
 # Leetcode 242
 # https://leetcode.com/problems/valid-palindrome/
@@ -30,3 +32,33 @@ def valid_palindrome_lc243(s: str) -> bool:
         rite -= 1
 
     return True
+
+def valid_palindrome_recursive_lc243(s: str) -> bool:
+    """A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+    Given a string s, return true if it is a palindrome, or false otherwise.
+
+    Recursive implementation.
+
+    :param s: The string to determine whether palindrome
+    :type s: str
+
+    :rtype: bool
+    :return: Boolean value corresponding to whether strings s is a palindrome, with regard to its alphanumeric chars
+    """
+    # testing if char is/not alnum is tedious recursively, outside scope of question;
+    # this line strips non alnums from s
+    s = ''.join(ch for ch in s if ch.isalnum()) 
+
+    # "" is a palindrome ; "a", where a is any character, is a palindrome
+    if len(s) == 0 or len(s) == 1:
+        return True
+
+    # if s has 2 chars, it is a palindrome if its chars are equal: 
+    if len(s) == 2:
+        return s[0].lower() == s[1].lower()
+
+    left: int = 0
+    rite: int = len(s) - 1
+
+    return valid_palindrome_recursive_lc243(s[left] + s[rite]) and valid_palindrome_recursive_lc243(s[left+1:rite])
