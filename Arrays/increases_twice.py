96e705df7a0895f3758169b3c4067db6caa0724f
Julian <juliangwillis@gmail.com>
2022-11-06 20:14:31 -0500

    Make increases_twice

---
+++ Arrays/increases_twice.py
@@ -0,0 +1,26 @@
+def increases_twice(nums: List[int]) -> int:
+    """Given an integer array nums, determine whether the array increases at least twice; that is, contains 3 ascending elements from its lowest to highest index.
+
+    Example: [0, 1, 2] increases at least twice and would return true. [0, 1, 1] increases only once and would return false.
+
+    Time complexity: O(n) [check entire list worst case]
+    Space complexity: O(1)
+
+    :param nums: The list of nums
+    :type nums: List[int]
+
+    :rtype: bool
+    :return: Boolean value of whether nums increases twice
+    """
+    smallest = float('inf')
+    second_smallest = smallest
+
+    for num in nums:
+        if num <= smallest:
+            smallest = num
+        elif num <= second_smallest:
+            second_smallest = num
+        else:
+            return True
+    
+    return False
