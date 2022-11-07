f9336bbc8fc79b0fa2c310fca6d5aaa40f9f1b03
Julian <juliangwillis@gmail.com>
2022-11-07 10:38:52 -0500

    Group anagrams, top k frequent elements(both unfinished)

--- Arrays/top_k_frequent_elements_lc347.py
+++ Arrays/top_k_frequent_elements_lc347.py
@@ -2,7 +2,7 @@
 # Leetcode 347
 # https://leetcode.com/problems/top-k-frequent-elements/
 
-# Unfinished
+# Unfinished (update comments)
 def top_k_frequent_elements(nums: list[int], k: int) -> list[int]:
     """Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
@@ -17,4 +17,22 @@ def top_k_frequent_elements(nums: list[int], k: int) -> list[int]:
     :rtype: list[int]
     :return: list of top k most frequent elements in nums
     """
+    occ_map = {}
+    for num in nums:
+        occ_map[num] = 1 + occ_map.get(num, 0)
+    
+    # index 0: nums that appear 0 times; 1: nums that appear 1 time, etc
+    count_to_num = [[] for i in range(len(nums) + 1)]
+
+    for num, occ in occ_map.items():
+        count_to_num[occ].append(num)
+
+    res = []
+
+    for i in range(len(count_to_num) - 1, 0, -1):
+        for num in count_to_num[i]:
+            res.append(num)
+        
+        if len(res) == k:
+                return res
     
--- Strings/group_anagrams_lc49.py
+++ Strings/group_anagrams_lc49.py
@@ -3,7 +3,7 @@
 # https://leetcode.com/problems/group-anagrams/
 
 #unfinished
-def group_anagrams_lc49(self, strs: list[str]) -> list[list[str]]:    
+def group_anagrams_lc49(strs: list[str]) -> list[list[str]]:    
     """Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
     An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.  
@@ -17,3 +17,20 @@ def group_anagrams_lc49(self, strs: list[str]) -> list[list[str]]:
     :rtype: list[list[str]
     :return: list containing sublists which group anagrams together, eg [['cat', 'tac'], ['rat', 'tar', 'art']]
     """
+    charocc_to_strs: {list[str] : list[str]} =  {} # map alphabet (index) occurance : its associated strings in strs
+
+    for word in strs:
+        this_charocc: list[int] = [0] * 26
+        # populate this_charocc
+        for char in word:
+            char_index: int = ord(char) - ord('a')
+            this_charocc[char_index] += 1
+
+        this_charocc_tuple: tuple = tuple(this_charocc) # lists cannot be keys
+
+        if this_charocc_tuple in charocc_to_strs:
+            charocc_to_strs[this_charocc_tuple].append(word)
+        else:
+            charocc_to_strs[this_charocc_tuple] = [word]
+
+    return charocc_to_strs.values()
