4500d752e20e5ca3fe3552d3785d0eb3cb81f273
Julian <juliangwillis@gmail.com>
2022-11-06 21:09:51 -0500

    Finish consecutive zero sum

--- Arrays/consecutive_zero_sum.py
+++ Arrays/consecutive_zero_sum.py
@@ -6,8 +6,8 @@ def consecutive_zero_sum(nums: list[int]) -> int:
     [0, 0, 0] -> 3: The sequence from i=0 to i=2 is the longest sequence which sums to 0.
     [1] -> -1: No sequence in this array sums to 0
 
-    Time complexity: O(
-    Space complexity: O(
+    Time complexity: O(n) [iterate multiple times thru entire arrays of length n]
+    Space complexity: O(n) [new hashmap and array]
 
     :param nums: The list of nums
     :type nums: List[int]
@@ -19,10 +19,37 @@ def consecutive_zero_sum(nums: list[int]) -> int:
     if not nums:
         return -1
 
-    # equivalent to cumulative_sum_array
+    # two types of consecutive 0 sums: those which start from beginning of list and those which do not.
+
+    # first build cuum sum array while finding longest cons zero sum which starts from beginning of list.
+    # building the cuum sum array is equivalent to cumulative_sum_array in Arrays.
     cuum_sum_list: list[int] = [nums[0]]
+    longest_zero_sum_from_beginning: int = 0 if cuum_sum_list[0] == 0 else -1
+
     for i in range(1, len(nums)):
         cuum_sum_here = nums[i] + cuum_sum_list[i-1]
         cuum_sum_list.insert(i, cuum_sum_here)
 
-    return cuum_sum_list
+        if cuum_sum_here == 0:
+            longest_zero_sum_from_beginning = i
+
+    # next find consecutive 0 sums which do not start from index 0.
+    # (these can be found by summing reflecting numbers in cuum_sum_list: ie from [-1, 3, 1] summing from these indices
+    # gives a consecutive zero sum of length 3)
+    # finding opposite in this way is nearly identical to two sum hashmap version leetcode also in Arrays.
+    seen_sum_map: {int: int} = {}
+    longest_zero_sum_from_mid: int = -1
+    
+    for i in range(0, len(cuum_sum_list)):
+        this_cuum_sum = cuum_sum_list[i]
+
+        if (this_cuum_sum * -1) in seen_sum_map:
+            this_length = i - seen_sum_map.get(this_cuum_sum * -1)
+            # max func will eliminate issues of ex. multiple cuum_sums surrounded by same number (eg [-1, 2, 1] and [1, 7, 3, -1] will find 4 as larger)
+            longest_zero_sum_from_mid = max(this_length, longest_zero_sum_from_mid) 
+
+        else:
+            seen_sum_map[this_cuum_sum] = i
+    
+    # now return the larger of the two sum lengths:
+    return max(longest_zero_sum_from_beginning, longest_zero_sum_from_mid)
