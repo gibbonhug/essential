75ab01c8d2f189f8d6fb3c77e0ad05a3bc883bf5
Julian <juliangwillis@gmail.com>
2022-11-07 19:17:54 -0500

    Create cols_of_matrix, unfinished

---
+++ 2D_Arrays/Basic_Tasks/cols_of_matrix.py
@@ -0,0 +1,11 @@
+# Basic Task: Iterate through the cols of a 2D Array (Matrix)
+
+def rows_of_matrix(matrix: list[list[int]]) -> list[list[int]]:
+    """Return a list containing the columns of a 2D matrix as lists.
+
+    :param matrix: The matrix to find cols from
+    :type matrix: list[list[int]]
+
+    :rtype: list[list[int]]
+    :return: List containing the cols of the matrix as lists.
+    """
