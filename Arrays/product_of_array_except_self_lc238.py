54cef57d1b3028ac183bc97f12247d9ece8f494c
Julian <juliangwillis@gmail.com>
2022-11-07 19:50:35 -0500

    Create product of array except self

---
+++ Arrays/product_of_array_except_self_lc238.py
@@ -0,0 +1,22 @@
+# Product Of Array Except Self
+# Leetcode 238
+# https://leetcode.com/problems/product-of-array-except-self/
+
+# Unfinished 
+def product_of_array_except_self_lc238(nums: list[int]) -> list[int]:
+    """Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+    You must write an algorithm that runs in O(n) time and without using the division operation.
+
+    Time complexity: O(n
+    Space complexity: O(
+
+    :param nums: The list of nums
+    :type nums: list[int]
+
+    :rtype: list[int]
+    :return: Array answer such that answer[i] is equal to the product of all the elements of nums except nums[i] 
+    """
+    
