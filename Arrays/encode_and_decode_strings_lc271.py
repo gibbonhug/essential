6c8aa777464151465b4467c7b78bad2770e23277
Julian <juliangwillis@gmail.com>
2022-11-16 09:28:32 -0500

    Create encode/decode strings (unfinished)

---
+++ Arrays/encode_and_decode_strings_lc271.py
@@ -0,0 +1,20 @@
+# Encode and Decode Strings
+# Leetcode 271
+# https://leetcode.com/problems/encode-and-decode-strings/
+
+# Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+# 
+# Please implement encode and decode 
+
+def decode(self, strs: list[str]) -> list[str]:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    
+
+def encode(self, strs:list[str]) -> list[str]:
+    """
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
