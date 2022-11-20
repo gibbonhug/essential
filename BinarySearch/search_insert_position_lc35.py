2c26755f0dfa72d7ae3158e9d7515c941e977f6d
Julian <juliangwillis@gmail.com>
2022-11-19 19:52:54 -0500

    Fix search insert position url comment

--- BinarySearch/search_insert_position_lc35.py
+++ BinarySearch/search_insert_position_lc35.py
@@ -1,6 +1,6 @@
 # Search Insert Position
 # Leetcode 35
-# https://leetcode.com/problems/search-insert-position/description/
+# https://leetcode.com/problems/search-insert-position/
 
 def search_insert_position_lc35(nums: list[int], target: int) -> int:
     """Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
