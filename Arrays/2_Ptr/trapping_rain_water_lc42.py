1b3661bca8472f51a86d01ecae581f251515a3c5
Julian <juliangwillis@gmail.com>
2022-11-22 21:10:14 -0500

    Create trapping rain water (unfinished)

---
+++ Arrays/2_Ptr/trapping_rain_water_lc42.py
@@ -0,0 +1,16 @@
+# Trapping Rain Water
+# Leetcode 42
+# https://leetcode.com/problems/trapping-rain-water/
+
+def trapping_rain_water_lc42(height: list[int]) -> int:
+    """Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+    Time Complexity:
+    Space Complexity:
+
+    :param height: The list of heights
+    :type nums: list[int]
+
+    :rtype: int
+    :return: The total amount of 'rain water' which will be trapped
+    """
