b7ffccf662fd9cd1663f3089987531ef640cfa49
Julian <juliangwillis@gmail.com>
2022-12-06 22:23:46 -0500

    Improve comments in new func

--- Arrays/Basic_Tasks/max_value_before_index.py
+++ Arrays/Basic_Tasks/max_value_before_index.py
@@ -7,6 +7,9 @@ def max_value_before_index(nums: list[int]) -> list[int]:
 
     Example: [0, 1, 4, 0, 3] -> [0, 0, 1, 4, 4]
 
+    (Function could easily be modified to traverse array backwards and produce maximum value seen
+    after current index)
+
     Time complexity: O(n)
     Space complexity: O(n)
 
@@ -14,7 +17,7 @@ def max_value_before_index(nums: list[int]) -> list[int]:
     :type nums: list[int]
 
     :rtype: list[int]
-    :return: Described list
+    :return: List of nums representing maxmimum value seen in nums before that index (index 0 of this list is 0)
     """
     res: list[int] = [0] * len(nums)
 
