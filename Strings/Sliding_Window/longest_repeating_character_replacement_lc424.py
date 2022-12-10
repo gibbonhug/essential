a1901bfd139cb55b14f6a59e924618314058c005
Julian <juliangwillis@gmail.com>
2022-12-10 16:43:22 -0500

    Finish longest repeating character replacement

--- Strings/Sliding_Window/longest_repeating_character_replacement_lc424.py
+++ Strings/Sliding_Window/longest_repeating_character_replacement_lc424.py
@@ -7,8 +7,10 @@ def longest_repeating_character_replacement_lc424(s: str, k: int) -> int:
 
     Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
-    Time complexity: O(n)
-    Space complexity: O(1)
+    Unoptimized solution (see youtube video)
+
+    Time complexity: O(26 * n)
+    Space complexity: O(26) [dictionary with 26 keys maximum]
 
     :param prices: The string to perform operation on
     :type prices: str
@@ -18,6 +20,26 @@ def longest_repeating_character_replacement_lc424(s: str, k: int) -> int:
     :rtype: int
     :return: Longest substring of s containing same char given at most k replacements
     """
+    char_occ: {str: int} = {}
+
+    l: int = 0
+
+    max_len: int = 0
+
+    for r in range(len(s)):
+        char_occ[s[r]] = char_occ.get(s[r], 0) + 1
+        len_max_occ: int = max(char_occ.values())
+
+        # r - l + 1: length of current substring
+
+        if (r - l + 1) - len_max_occ > k:
+            # shrink window
+            char_occ[s[l]] -= 1
+            l += 1
+
+        max_len = max(max_len, (r - l + 1))
+
+    return max_len
 
 def longest_repeating_character_replacement_lc424_bruteforce(s: str, k: int) -> int:
     """You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
@@ -27,7 +49,7 @@ def longest_repeating_character_replacement_lc424_bruteforce(s: str, k: int) -> 
     Brute Force Solution
 
     Time complexity: O(n^2)
-    Space complexity: O(n)
+    Space complexity: O(26) [dictionary with 26 keys maximum]
 
     :param prices: The string to perform operation on
     :type prices: str
@@ -59,7 +81,7 @@ def longest_repeating_character_replacement_lc424_bruteforce(s: str, k: int) -> 
                 continue
 
             # find occ of char which appears most often in this substring
-            len_max_occ: int = this_map[max(this_map, key=this_map.get)]
+            len_max_occ: int = max(this_map.values())
 
             num_chars_to_replace: int = this_len - len_max_occ
 
