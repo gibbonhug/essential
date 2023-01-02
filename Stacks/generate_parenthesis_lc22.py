57c1908040fb045282585bde97ebc35078319817
Julian <juliangwillis@gmail.com>
2023-01-02 08:01:07 -0500

    New file for new problem, remove old Unfinished comment

--- Stacks/generate_parenthesis_lc22.py
+++ Stacks/generate_parenthesis_lc22.py
@@ -2,8 +2,6 @@
 # Leetcode 22
 # https://leetcode.com/problems/generate-parentheses/
 
-# unfinished
-
 def generate_parenthesis_lc22(n: int) -> list[str]:
     """Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
