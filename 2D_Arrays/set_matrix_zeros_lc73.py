b0175f760e89a4e77299d0a5c77a4afe2015a660
Julian <juliangwillis@gmail.com>
2023-01-02 17:59:39 -0500

    Create set matrix zeros

---
+++ 2D_Arrays/set_matrix_zeros_lc73.py
@@ -0,0 +1,18 @@
+# Set Matrix Zeros
+# Leetcode 73
+# https://leetcode.com/problems/set-matrix-zeroes/
+
+def set_matrix_zeros_lc73(matrix: list[list[int]]) -> None:
+    """Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+    You must do it in place.
+
+    Time Complexity: 
+    Space Complexity:
+
+    :param matrix: The matrix to modify in-place
+    :type matrix: list[list[int]]
+
+    :rtype: None
+    :return: Nothing, modify in-place
+    """
