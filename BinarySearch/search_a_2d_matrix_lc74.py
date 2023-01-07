17a3312f03a742e2b3e9e8d4d616c77ce1ef7df0
Julian <juliangwillis@gmail.com>
2023-01-06 21:40:39 -0500

    Remove old comments from search a 2d matrix

--- BinarySearch/search_a_2d_matrix_lc74.py
+++ BinarySearch/search_a_2d_matrix_lc74.py
@@ -2,8 +2,6 @@
 # Leetcode 74
 # https://leetcode.com/problems/search-a-2d-matrix/
 
-# Unfinished (no comments)
-
 def search_a_2d_matrix_lc74(matrix: list[list[int]], target: int) -> bool:
     """You are given an m x n integer matrix matrix with the following two properties:
 
