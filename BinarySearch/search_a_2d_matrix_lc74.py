f6b5cafa752e17f097690578eeb0313556ecee53
Julian <juliangwillis@gmail.com>
2023-01-06 21:35:34 -0500

    Create search a 2d matrix

---
+++ BinarySearch/search_a_2d_matrix_lc74.py
@@ -0,0 +1,10 @@
+# Search a 2D Matrix
+# Leetcode 74
+# https://leetcode.com/problems/search-a-2d-matrix/
+
+# Unfinished (no comments)
+
+def search_a_2d_matrix_lc74(matrix: list[list[int]], target: int) -> bool:
+    """
+    """
+    
